c6bd8afbe964587accbdde68b67d2372637545b9
Rumata <Rumatadeestor@gmail.com>
2016-04-17 19:39:51 +0300

    update

--- index.js
+++ index.js
@@ -1,13 +1,44 @@
 var express = require('express');
+// var app = express();
+ 
+// app.get('/user/:id([0-9]+)', function(req, res, next){
+// 	var users = ["Alina","Vlad","Vova"];
+// 	userId = req.params.id;
+// 	if (users[userId]) {
+//     	res.send("user №"+userId+" "+users[userId]+" is here.");
+// 	} else {
+// 		res.send("Can not find such user");
+// 	}
+// });
+ 
+// app.listen(8080);
 
+// var express = require('express');
+// var app = express();
+
+ 
+// app.get('/user/:id([0-9]+)', function(req, res, next){
+// 	var users = ["Alina","Vlad","Vova"];
+// 	userId = req.params.id;
+// 	if (users[userId]) {
+//     	res.render('index', {
+//     title: users[userId],
+//     message: "Information"
+// 		});
+// 	} else {
+// 		res.send("Can not find such user");
+// 	}
+// });
 var app = express();
+app.set('view engine', 'jade');
+app.engine('html', require('ejs').renderFile);
 
+app.use('/user/0', function (req, res, next) {
+	res.render('index.html');
+}) 
 app.use('/user/1', function (req, res, next) {
-	res.send("Юзер 1");
+	res.send("Да это же AJAX :)");
 }) 
 
-app.get('/', function (req, res, next) {
-	res.send("This is my app!");
-})
 
 app.listen(process.env.PORT || 8080);
